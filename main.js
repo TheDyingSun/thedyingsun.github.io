e9b50414ae9b1ab8369fa52a667a1e52521afec5
TheDyingSun <thedyingsun.ng@gmail.com>
2022-04-19 22:43:58 -0400

    added previous files from private repo

---
+++ main.js
@@ -0,0 +1,8 @@
+const general_info_popup = document.querySelector('.general-info')
+
+general_info_popup.addEventListener('click', (e) => {
+
+    document.querySelector('pop-up').style.visibility = visible;
+
+
+})
